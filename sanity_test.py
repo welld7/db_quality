3e4327b50bfb53afa3af4dfd20981b195e09dcd0
Evgeny Rovinskiy <evgeny.rovinskiy@glidewelldental.com>
2018-01-15 19:28:46 +0300

    different date+int

--- sanity_test.py
+++ sanity_test.py
@@ -77,13 +77,34 @@ def test_avg_int_one_date(handle_connection_and_drop, int_input):
 
     assert int_input == get_int_avg_in_status_table_by_rowid(conn, rowid_inserted)
 
+@pytest.mark.sanity
+@pytest.mark.parametrize("int_input", [1,2])
+def test_avg_int_different_load_dates(handle_connection_and_drop, int_input):
+    conn = handle_connection_and_drop  # create_connection(database)
+
+    row1 = ('2017-01-05', randint(0, 1000000), 0, 2.0, "hi", '2013-01-05')
+    insert_new_row(conn, row1)
+
+    row2 = ('2017-01-05', randint(0, 1000000), int_input, 2.0, "hi", '2013-01-05')
+    insert_new_row(conn, row2)
+
+    row3 = ('2017-01-05', randint(0, 1000000), 2 * int_input, 2.0, "hi", '2013-01-05')
+    insert_new_row(conn, row3)
+
+    row4 = ('2017-01-04', randint(0, 1000000), 1000000, 2.0, "hi", '2013-01-05')
+    insert_new_row(conn, row1)
+
+    #the avg_int isn't changed by the other day row
+    rowid_inserted = add_day_status_row(conn, '2017-01-05')
+    print_table(conn)
+
+    assert int_input == get_int_avg_in_status_table_by_rowid(conn, rowid_inserted)
 
 @pytest.mark.sanity
-@pytest.mark.par_connection_and_drop #create_connection(database)
-    ametrize("float_input", [-1., 1.7976931348623157e+30, 1029.])
+@pytest.mark.parametrize("float_input", [-1., 1.7976931348623157e+30, 1029.])
+def test_avg_float_one_date(handle_connection_and_drop, float_input):
+    conn = handle_connection_and_drop #create_connection(database)
 
-    def test_avg_float_one_date(handle_connection_and_drop, float_input):
-        conn = handle
     row1 = ('2017-01-05', randint(0, 1000000), 0, 2.0, "hi", '2013-01-05')
     insert_new_row(conn, row1)
 
@@ -112,15 +133,15 @@ def test_count(handle_connection_and_drop, number_of_rows):
     assert number_of_rows == get_count(conn, day, get_next_day(day))
 
 
-@pytest.mark.sanity
-@pytest.mark.parametrize("number_of_rows", [0, 1, 300])
-def test_count(handle_connection_and_drop, number_of_rows):
-    conn = handle_connection_and_drop #create_connection(database)
-    if conn is not None:
-        day = '2017-01-05'
-        for _ in range(number_of_rows):
-            row = (day, 0, 0, 0, "", day)
-            insert_new_row(conn, row)
-
-    assert number_of_rows == get_count(conn, day, get_next_day(day))
+#@pytest.mark.sanity
+#@pytest.mark.parametrize("number_of_rows", [0, 1, 300])
+#def test_count2(handle_connection_and_drop, number_of_rows):
+#    conn = handle_connection_and_drop #create_connection(database)
+#    if conn is not None:
+#        day = '2017-01-05'
+#        for _ in range(number_of_rows):
+#            row = (day, 0, 0, 0, "", day)
+#            insert_new_row(conn, row)
+#
+#    assert number_of_rows == get_count(conn, day, get_next_day(day))
 
